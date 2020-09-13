007646ef0bb56c7a7b583306cd8100c23ffd3163
Shahab Ali <mrshahabali@gmail.com>
2020-09-13 16:27:25 +0500

    First commit

---
+++ LibraryMid2/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LibraryMid2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Published { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
